7a34f8bec0d86c8bd9735ec4a7f675ebc969ea85
namvv <nam.vv@archetype-group.com>
2019-10-16 16:12:11 +0700

    Init project

---
+++ application/controllers/guestbook.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nam Vo
+ * Date: 10/16/2019
+ * Time: 11:41 AM
+ */
---
+++ application/views/guestbook/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nam Vo
+ * Date: 10/16/2019
+ * Time: 11:42 AM
+ */
+
